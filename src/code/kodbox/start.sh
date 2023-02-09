bc68925f4e6dc2cfb2cbbf651cdd35bf1e1bb362
yusha.xy <yusha.xy@alibaba-inc.com>
2023-02-09 15:20:44 +0800

    unzip in code

--- src/code/kodbox/start.sh
+++ src/code/kodbox/start.sh
@@ -3,11 +3,10 @@ set +e
 
 echo "start the sh"
 if [ ! -d /mnt/nas/.kodbox-1.35.031 ]; then
-  wget http://images.devsapp.cn/application/kodbox/kodbox-1.35.031.zip -O /mnt/nas/kodbox-1.35.031.zip
-  cd /mnt/nas && unzip kodbox-1.35.031.zip && mv kodbox-1.35.031 .kodbox-1.35.031 && rm kodbox-1.35.031.zip
+  wget http://images.devsapp.cn/application/kodbox/kodbox-1.35.031.zip -O /code/kodbox-1.35.031.zip
+  cd /code && unzip kodbox-1.35.031.zip && mv kodbox-1.35.031 .kodbox-1.35.031 && rm kodbox-1.35.031.zip && chown -R root:root  ./.kodbox-1.35.031 && mv ./.kodbox-1.35.031 /mnt/nas
 fi
 
-chown -R root:root  /mnt/nas/.kodbox-1.35.031
 
 ln -s /mnt/nas/.kodbox-1.35.031 /wwwroot
 mkdir -p /wwwroot/sessions
