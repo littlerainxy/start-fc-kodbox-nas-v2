876fbfc80e4f2a7e59d9557bdcc5d3af3b3c4507
yusha.xy <yusha.xy@alibaba-inc.com>
2023-02-09 17:42:51 +0800

    fix path

--- src/code/kodbox/start.sh
+++ src/code/kodbox/start.sh
@@ -4,7 +4,7 @@ set +e
 echo "start the sh"
 if [ ! -d /mnt/nas/.kodbox-1.35.031/ ]; then
   wget http://images.devsapp.cn/application/kodbox/kodbox-1.35.031.zip -O /mnt/nas/kodbox-1.35.031.zip
-  cd /mns/nas && unzip kodbox-1.35.031.zip && mv kodbox-1.35.031 .kodbox-1.35.031 && rm kodbox-1.35.031.zip && chmod -R 777 .kodbox-1.35.031
+  unzip /mnt/nas/kodbox-1.35.031.zip && mv /mnt/nas/kodbox-1.35.031 /mnt/nas/.kodbox-1.35.031 && rm /mnt/nas/kodbox-1.35.031.zip && chmod -R 777 /mnt/nas/.kodbox-1.35.031
 fi
 
 ln -s /mnt/nas/.kodbox-1.35.031 /wwwroot
