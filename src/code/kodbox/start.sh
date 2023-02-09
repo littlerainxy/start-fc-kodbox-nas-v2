8180fa4aed8d00a56b923a6964f841dfdc62172c
yusha.xy <yusha.xy@alibaba-inc.com>
2023-02-09 17:27:40 +0800

    chomod 777

--- src/code/kodbox/start.sh
+++ src/code/kodbox/start.sh
@@ -2,11 +2,10 @@
 set +e
 
 echo "start the sh"
-if [ ! -d /mnt/nas/.kodbox-1.35.031 ]; then
-  wget http://images.devsapp.cn/application/kodbox/kodbox-1.35.031.zip -O /code/kodbox-1.35.031.zip
-  cd /code && unzip kodbox-1.35.031.zip && mv kodbox-1.35.031 .kodbox-1.35.031 && rm kodbox-1.35.031.zip && chown -R root:root  ./.kodbox-1.35.031 && mv ./.kodbox-1.35.031 /mnt/nas
+if [ ! -d /mnt/nas/.kodbox-1.35.031/ ]; then
+  wget http://images.devsapp.cn/application/kodbox/kodbox-1.35.031.zip -O /mnt/nas/kodbox-1.35.031.zip
+  cd /mns/nas && unzip kodbox-1.35.031.zip && mv kodbox-1.35.031 .kodbox-1.35.031 && rm kodbox-1.35.031.zip && chmod -R 777 .kodbox-1.35.031
 fi
-
 
 ln -s /mnt/nas/.kodbox-1.35.031 /wwwroot
 mkdir -p /wwwroot/sessions
